237708e35cfc8e62be06e9e811ad3691b2f54468
Avashesh07 <155659344+Avashesh07@users.noreply.github.com>
2024-11-05 20:30:32 +0200

    Update SoftwareSkill.js

--- src/components/softwareSkills/SoftwareSkill.js
+++ src/components/softwareSkills/SoftwareSkill.js
@@ -1,26 +1,37 @@
+// src/components/softwareSkills/SoftwareSkill.js
 import React from "react";
 import "./SoftwareSkill.scss";
-import {skillsSection} from "../../portfolio";
+import { skillsSection } from "../../portfolio";
 
 export default function SoftwareSkill() {
   return (
-    <div>
-      <div className="software-skills-main-div">
-        <ul className="dev-icons">
-          {skillsSection.softwareSkills.map((skills, i) => {
-            return (
-              <li
-                key={i}
-                className="software-skill-inline"
-                name={skills.skillName}
-              >
-                <i className={skills.fontAwesomeClassname}></i>
-                <p>{skills.skillName}</p>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+    <div className="software-skills-main-div">
+      <ul className="dev-icons">
+        {skillsSection.softwareSkills.map((skill, i) => {
+          return (
+            <li
+              key={i}
+              className="software-skill-inline"
+              name={skill.skillName}
+              title={skill.skillName} // Optional: Adds tooltip on hover
+            >
+              {skill.fontAwesomeClassname ? (
+                <i
+                  className={skill.fontAwesomeClassname}
+                  aria-hidden="true"
+                ></i>
+              ) : skill.svgPath ? (
+                <img
+                  src={skill.svgPath}
+                  alt={skill.skillName}
+                  className="skill-svg-icon"
+                />
+              ) : null}
+              <p>{skill.skillName}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
