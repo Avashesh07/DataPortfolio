a754cdd49f341862f2415cd67caa6204f892656a
Avashesh07 <155659344+Avashesh07@users.noreply.github.com>
2024-11-07 13:10:13 +0200

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -386,20 +386,14 @@ const achievementSection = {
 const blogSection = {
   title: "Blogs",
   subtitle:
-    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+    "With Love for solving problems, I love to write about the problems I have come across and document the process of how I am attempting to solve them.",
   displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
   blogs: [
     {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      url: "https://medium.com/@avashesh.work/engineering-a-smart-scheduling-algorithm-for-450-cricket-matches-amidst-myriad-constraints-part-66c653250ff4",
+      title: "Engineering a Smart Scheduling Algorithm for 450+ Cricket Matches (Part 1)",
       description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
-    },
-    {
-      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
-      description:
-        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+        "This article covers my iterative journey, highlighting the challenges faced, solutions devised, and the eventual triumph (hopefully) of crafting a comprehensive scheduling model."
     }
   ],
   display: true // Set false to hide this section, defaults to true
